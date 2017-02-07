f977c26e1393c0921c6de1894f768ee38393964c
Héctor Salazar <h.salazar509@gmail.com>
2017-02-07 14:09:27 -0600

    Creates user model - no specs yet

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  enum role: [:standard, :admin, :super_admin]
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
