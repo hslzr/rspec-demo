172b7ed86b4db4e6ac4e348f80642a456168ad6f
Héctor Salazar <h.salazar509@gmail.com>
2017-02-07 16:15:19 -0600

    Updates spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  
+  before(:each) do
+    @user = create(:user)
+  end
+
 
   describe '#validations' do
     context 'name' do
-      it 'validates presence' do
-        
+      it 'is present' do
+        expect(@user.name).not_to be_empty
       end
-      it 'validates it only contains letters' do
-        
+
+      it 'only contains letters' do
+        expect(@user.name).to match /\A[a-z]+\z/i
+      end
+    end
+
+    context 'email' do
+      it 'is present' do
+        expect(@user.email).not_to be_empty
+      end
+
+      it 'belongs to Helicon employee' do
+        expect(@user.email).to match /(\w\.?\w+)@hlcn.mx\z/
       end
     end
   end
-
 end
