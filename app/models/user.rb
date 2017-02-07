3f746085f9e72a6f0804df1ee9f9e7a7bf9a609f
Héctor Salazar <h.salazar509@gmail.com>
2017-02-07 14:19:18 -0600

    Adds validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true,
+                    format: { with: /\A[a-z]+\z/i, message: "Letters only" }
+
+  validates :email, presence: true,
+                    format: { with: /(\w+.?\w+)@hlcn.mx\z/i, message: "Helicon only" }
+
 end
